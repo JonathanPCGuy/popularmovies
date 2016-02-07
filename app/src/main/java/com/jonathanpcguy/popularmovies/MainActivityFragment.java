32081eca9bffacad3282585ddf9274bbe6653b15
Jonathan Lam <jlampcguy@gmail.com>
2016-02-06 20:22:50 -0600

    Testing out v1 of secrets

--- app/src/main/java/com/jonathanpcguy/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/jonathanpcguy/popularmovies/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +18,11 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        TextView textView = (TextView)rootView.findViewById(R.id.debug_text_view);
+        textView.setText(getString(R.string.the_movie_db_api_key));
+
+        return rootView;
     }
 }
